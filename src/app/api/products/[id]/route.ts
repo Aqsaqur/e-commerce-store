bc64027300e1df47a8fead8371f08e4293f70b23
Aqsa Qur <aqsaqurayshi@gmail.com>
2025-01-08 00:42:11 +0500

    i made changes

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { products } from '@/lib/data';
 
-export async function GET(
+export async function generateStaticParams(
   request: Request,
   { params }: { params: { id: string } }
 ) {
