dd93283189bf9c8343ee1be8d61dd2554c4d9450
Aqsa Qur <aqsaqurayshi@gmail.com>
2025-01-08 00:46:39 +0500

    changes

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,15 +1,15 @@
-import { NextResponse } from 'next/server';
-import { products } from '@/lib/data';
+// import { NextResponse } from 'next/server';
+// import { products } from '@/lib/data';
 
-export async function generateStaticParams(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const product = products.find((p) => p.id === params.id);
+// export async function generateStaticParams(
+//   request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const product = products.find((p) => p.id === params.id);
   
-  if (!product) {
-    return new NextResponse('Product not found', { status: 404 });
-  }
+//   if (!product) {
+//     return new NextResponse('Product not found', { status: 404 });
+//   }
 
-  return NextResponse.json(product);
-}
+//   return NextResponse.json(product);
+// }
